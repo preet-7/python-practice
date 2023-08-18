9e798601d067dd31cfe0ebefd30cdc7702d0187c
preet-7 <preet3.14159@gmail.com>
2023-08-18 10:48:26 +0530

    new file:   Day1/main.py

---
+++ Day1/main.py
@@ -0,0 +1,60 @@
+# print("Hello World!")
+
+# print("Day-1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
+
+# print("hello \nworld")
+
+# print("Hello"+ " " +"Preet")
+
+# #Fix the code below 👇
+# print("Day 1 - String Manipulation")
+# print('String Concatenation is done with the "+" sign.')
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+# #input("what is your name?")
+
+# print("Hello " + input("what is your name")) 
+
+# print(len(input("What is your name? ")))
+
+# name= input("what is your name? ")
+# print(name)
+
+# name="jack"
+# print(name)
+# name='angela'
+# print(name)
+
+# name= input("what is your name? ")
+# length=len(name)
+# print(length)
+
+# # 🚨 Don't change the code below 👇
+# a = input("a: ")
+# b = input("b: ")
+# # 🚨 Don't change the code above 👆
+
+# ####################################
+# #Write your code below this line 👇
+
+# t=a
+# a=b
+# b=t 
+
+# #Write your code above this line 👆
+# ####################################
+
+# # 🚨 Don't change the code below 👇
+# print("a: " + a)
+# print("b: " + b)
+
+
+
+# #Day 1 Project: Band Name Generator
+# print("Welcome to the band name generator.")
+# city= input("which city did you grow up in?\n")
+# pet= input("what is the name of a pet?\n")
+# print("Your band name could be "+ city +" "+ pet)
