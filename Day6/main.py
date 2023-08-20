8e4c760c92a297b9e0a5dd8717d31d5caca9a7a0
preet-7 <preet3.14159@gmail.com>
2023-08-20 22:59:27 +0530

    new file:   Day6/main.py

---
+++ Day6/main.py
@@ -0,0 +1,146 @@
+# print("hello")
+# num_char=len("hello")
+# print(num_char)
+
+# def my_function():
+#     print("hello")
+#     print("bye")
+
+# my_function()
+
+#https://www.udemy.com/course/100-days-of-code/learn/lecture/19115132#content
+
+# def turn_arround():
+#     turn_left()
+#     turn_left()
+
+# move()
+# move()
+# move()
+# turn_arround()
+# move()
+# move()
+# move()
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# turn_left()
+# move()
+# turn_right()
+# move()
+# turn_right()
+# move()
+# turn_right()
+# move()
+
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# for step in range(6):
+#     jump()
+
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# while not at_goal():
+#     jump()
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+
+
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def jump():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# while front_is_clear():
+#     move()
+# turn_left()
+
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
+        
+    
+
+    
+
+
+
+    
