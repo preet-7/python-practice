fdd3945203410ec3fae6c6e83e6ba620f6746bf9
preet-7 <preet3.14159@gmail.com>
2023-08-27 23:16:22 +0530

    added Day17

---
+++ Day17/test.py
@@ -0,0 +1,66 @@
+# class User:
+#     pass
+#
+# user_1 = User()
+# user_1.id = "001"
+# user_1.username = "angela"
+# print(user_1.username)
+#
+# user_2 = User()
+# user_2.id = "002"
+# user_2.username = "jack"
+# print(user_2.username)
+
+# class User:
+#     def __init__(self):
+#         print("new user being created")
+#
+# user_1 = User()
+# user_1.id = "001"
+# user_1.username = "angela"
+# print(user_1.username)
+#
+# user_2 = User()
+# user_2.id = "002"
+# user_2.username = "jack"
+# print(user_2.username)
+
+
+# class User:
+#     def __init__(self,user_id, username):
+#         print("new user being created")
+#         self.id = user_id
+#         self.username = username
+#         self.followers = 0
+#
+# user_1 = User("001", "angela")
+# print(user_1.id)
+# print(user_1.username)
+# print(user_1.followers)
+#
+# user_2 = User("002", "jack")
+# print(user_2.id)
+# print(user_2.username)
+
+
+class User:
+    def __init__(self,user_id, username):
+        print("new user being created")
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers +=1
+        self.following +=1
+
+
+user_1 = User("001", "angela")
+user_2 = User("002", "jack")
+
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
