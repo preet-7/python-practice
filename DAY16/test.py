850ed69570acbaaecd753cff8deedda80c89aa8a
preet-7 <preet3.14159@gmail.com>
2023-08-27 17:36:29 +0530

    added Day16

---
+++ DAY16/test.py
@@ -0,0 +1,23 @@
+# import turtle
+# timmy = turtle.Turtle()
+
+# from turtle import Turtle, Screen
+# tinny = Turtle()
+# print(tinny)
+# tinny.shape("turtle")
+# tinny.color("coral")
+# tinny.forward(100)
+# print(tinny.speed())
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+# from prettytable import PrettyTable
+# table = PrettyTable()
+# table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+# table.add_column("Type", ["Electric", "Water", "Fire"])
+# print(table)
+# table.align = "l"
+# print(table)
+
