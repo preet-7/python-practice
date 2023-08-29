832fd34b56a5e03636e34c8d7ce3d93037ca3095
preet-7 <preet3.14159@gmail.com>
2023-08-29 23:43:39 +0530

    added Day21

---
+++ Day21/test.py
@@ -0,0 +1,77 @@
+# class Animal:
+#     def __init__(self):
+#         self.num_eyes = 2
+#
+#     def breathe(self):
+#         print("inhale, exhale")
+#
+# class Fish:
+#     def swim(self):
+#         print("moving in the water")
+#
+#
+# nemo =Fish()
+# nemo.swim()
+
+
+
+
+
+
+
+# class Animal:
+#     def __init__(self):
+#         self.num_eyes = 2
+#
+#     def breathe(self):
+#         print("inhale, exhale")
+#
+#
+# class Fish(Animal):
+#
+#     def __init__(self):
+#         super().__init__()
+#     def swim(self):
+#         print("moving in the water")
+#
+#
+# nemo = Fish()
+# nemo.swim()
+# nemo.breathe()
+# print(nemo.num_eyes)
+
+
+
+#
+# class Animal:
+#     def __init__(self):
+#         self.num_eyes = 2
+#
+#     def breathe(self):
+#         print("inhale, exhale")
+#
+#
+# class Fish(Animal):
+#
+#     def __init__(self):
+#         super().__init__()
+#
+#     def breathe(self):
+#         super().breathe()
+#         print("doing under the water")
+#
+#     def swim(self):
+#         print("moving in the water")
+#
+#
+#
+# nemo = Fish()
+# nemo.breathe()
+
+# piano_keys = ["a","b","c","d","e","f","g"]
+# print(piano_keys[2:5])
+# print(piano_keys[2:])
+# print(piano_keys[:5])
+# print(piano_keys[2:5:2])
+# print(piano_keys[::2])
+# print(piano_keys[::-1])
