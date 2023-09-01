e9c3342b71ff7e8c713afc7f2a02b16d46129561
preet-7 <preet3.14159@gmail.com>
2023-09-01 11:42:32 +0530

    added Day24

---
+++ Day24/test.py
@@ -0,0 +1,17 @@
+# file = open("file.txt")
+# contents = file.read()
+# print(contents)
+# file.close()
+
+# with open("file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+# with open("file.txt", mode="w") as file:
+#     file.write("next text")
+
+# with open("file.txt", mode="a") as file:
+#     file.write("\n yes another text")
+#
+# with open("newfile.txt", mode="w") as file:
+#     file.write("new text")    
