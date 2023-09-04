7319c12ea370d92277397657c75d9f7e637f0013
preet-7 <preet3.14159@gmail.com>
2023-09-04 21:28:15 +0530

    added Day26

--- Day27/main.py
+++ Day27/main.py
@@ -17,7 +17,6 @@
 # calculate(add=3, multiply=5)
 
 
-
 # def calculate(n, **kwargs):
 #     print(kwargs)
 #     n += kwargs["add"]
@@ -25,7 +24,6 @@
 #     print(n)
 #
 # calculate(2, add=3, multiply=5)
-
 
 
 # class Car:
@@ -52,7 +50,6 @@
 # print(my_car.make, my_car.model)
 
 
-
 # import tkinter
 #
 # window = tkinter.Tk()
@@ -62,8 +59,6 @@
 # #label
 # my_label = tkinter.Label(text="I am a Label", font=("Arial", 16, "bold"))
 # my_label.pack(side="top")
-
-
 
 
 # from tkinter import *
@@ -186,12 +181,6 @@
 #
 
 
-
-
-
-
-
-
 # from tkinter import *
 #
 # window = Tk()
@@ -227,3 +216,37 @@
 # input.grid(column=2,row=2)
 #
 # window.mainloop()
+
+
+from tkinter import *
+
+def miles_to_km():
+    miles = float(miles_input.get())
+    km = round(miles * 1.689)
+    kilometer_result_label.config(text=f"{km}")
+
+window = Tk()
+window.title("Miles to Kilometer Converter")
+window.config(padx=20, pady=20)
+
+miles_input = Entry(width=7)
+miles_input.grid(column=1, row=0)
+
+
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
+
+is_equal_label = Label(text="is equal to")
+is_equal_label.grid(column=0, row=1)
+
+kilometer_result_label = Label(text="0")
+kilometer_result_label.grid(column=1, row=1)
+
+kilometer_label = Label(text="Km")
+kilometer_label.grid(column=2, row=1)
+
+calculate_button = Button(text="Calculate", command=miles_to_km)
+calculate_button.grid(column=1, row=2)
+
+
+window.mainloop()
